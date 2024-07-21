b2932a49721a2ba93b9e0df4d6da54343c8da185
Ivan <ipkozyrin@gmail.com>
2024-07-20 23:41:37 -0300

    WIP

--- src/.vitepress/props.js
+++ src/.vitepress/props.js
@@ -1,7 +1,7 @@
 export const PROPS = {
   hostname: "https://blog.antifem-move.org",
   repo: "https://github.com/bozonx/antifem-blog",
-  siteUrl: "https://antifem-move",
+  siteUrl: "https://antifem-move.org",
   footerCopyright: "Copyright © 2024-present Antifem movement.",
   perPage: 10,
   paginationMaxItems: 7,
