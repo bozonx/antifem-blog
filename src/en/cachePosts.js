694cc238e47303038dbbb2e2e4b87bd95a6320ec
Ivan <ipkozyrin@gmail.com>
2025-09-06 19:55:00 -0300

    change

--- src/en/cachePosts.js
+++ src/en/cachePosts.js
@@ -1,18 +1,10 @@
 import path from "path";
 import { loadPosts } from "vitepress-sls-blog-tmpl/loadPosts.js";
-import { POSTS_DIR } from "vitepress-sls-blog-tmpl/constants.js";
 
-let posts = [];
-const postsDir = path.resolve(
-  path.dirname(import.meta.url.replace("file://", "")),
-  "./",
-  POSTS_DIR
-);
+const localeDir = path.dirname(import.meta.url.replace("file://", ""));
 
 export default async function (ignoreCache = false) {
-  if (posts.length > 0 && !ignoreCache) return posts;
-
-  posts = await loadPosts(postsDir);
+  const posts = await loadPosts(localeDir, ignoreCache);
 
   return posts;
 }
