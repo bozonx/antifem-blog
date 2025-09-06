e02a1bd702ccfe9216ad1fb5dadce8871e84e30a
Ivan <ipkozyrin@gmail.com>
2025-09-06 19:39:09 -0300

    add

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -2,40 +2,52 @@ import { defineConfig } from "vitepress";
 import blogConfigBase from "vitepress-sls-blog-tmpl/blogConfigBase.js";
 import { loadBlogLocale } from "vitepress-sls-blog-tmpl/blogConfigHelper.js";
 import { PROPS } from "./props.js";
+// import enCachePosts from "../en/cachePosts.js";
+// import ruCachePosts from "../ru/cachePosts.js";
 
 const ru = loadBlogLocale("ru", __filename, PROPS);
 const en = loadBlogLocale("en", __filename, PROPS);
 const configBase = blogConfigBase(PROPS, en);
 
-export default defineConfig({
-  ...configBase,
-  locales: {
-    ...configBase.locales,
-    en: { lang: "en-US", ...en },
-    ru: { lang: "ru-RU", ...ru },
-  },
-  themeConfig: {
-    ...configBase.themeConfig,
-    sidebarLogoSrc: "/img/sidebar-logo.webp",
-    paginationMaxItems: 7,
-  },
-  head: [
-    ...configBase.head,
-    // do not recognize telephone numbers on the page
-    ["meta", { name: "format-detection", content: "telephone=no" }],
+export default async () => {
+  // const postsEn = await enCachePosts();
+  // const postsRu = await ruCachePosts();
 
-    [
-      "script",
-      {
-        async: "",
-        src: "https://www.googletagmanager.com/gtag/js?id=G-M9CWSVHKYS",
-      },
-    ],
-    [
-      "script",
-      {},
-      `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
+  return defineConfig({
+    ...configBase,
+
+    // posts: {
+    //   en: postsEn,
+    //   ru: postsRu,
+    // },
+    locales: {
+      ...configBase.locales,
+      en: { lang: "en-US", ...en },
+      ru: { lang: "ru-RU", ...ru },
+    },
+    themeConfig: {
+      ...configBase.themeConfig,
+      sidebarLogoSrc: "/img/sidebar-logo.webp",
+      paginationMaxItems: 7,
+    },
+    head: [
+      ...configBase.head,
+      // do not recognize telephone numbers on the page
+      ["meta", { name: "format-detection", content: "telephone=no" }],
+
+      [
+        "script",
+        {
+          async: "",
+          src: "https://www.googletagmanager.com/gtag/js?id=G-M9CWSVHKYS",
+        },
+      ],
+      [
+        "script",
+        {},
+        `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
        gtag('config', 'G-M9CWSVHKYS');`,
+      ],
     ],
-  ],
-});
+  });
+};
--- src/en/author/[id]/[page].md
+++ src/en/author/[id]/[page].md
@@ -9,18 +9,19 @@ head:
 <script setup>
 import AuthorDetails from 'vitepress-sls-blog-tmpl/AuthorDetails.vue'
 import { useData } from 'vitepress'
-import { data } from '../../loadPosts.data.js'
+import { inject } from 'vue'
 import { PROPS } from "../../../.vitepress/props.js";
 
-const { theme, params } = useData()
+const { theme, params, localeIndex } = useData()
+const posts = inject('posts')
 </script>
 
 # {{theme.authors.find((item) => item.id === params.id)?.name}}
 
 <AuthorDetails
-  :allPosts="data.posts"
+  :allPosts="posts[localeIndex]"
   :authorId="params.id"
   :curPage="params.page"
   :perPage="PROPS.perPage"
-  :paginationMaxItems="PROPS.paginationMaxItems"
+  :paginationMaxItems="theme.paginationMaxItems"
 />
