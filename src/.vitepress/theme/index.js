fc4a71f3b4d338f08bbb5521a94ae296962fd73a
Ivan <ipkozyrin@gmail.com>
2025-09-16 08:14:28 -0300

    add

--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -1,5 +1,6 @@
 import Layout from "./Layout.vue";
 import { YouTubeEmbed } from "@miletorix/vitepress-youtube-embed";
+import FileDownload from "vitepress-sls-blog-tmpl/FileDownload.vue";
 import "@miletorix/vitepress-youtube-embed/style.css";
 import "vitepress-sls-blog-tmpl/styles.css";
 import "./styles.css";
@@ -8,5 +9,6 @@ export default {
   Layout,
   enhanceApp(ctx) {
     ctx.app.component("YouTubeEmbed", YouTubeEmbed);
+    ctx.app.component("FileDownload", FileDownload);
   },
 };
