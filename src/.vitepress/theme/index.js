f31622596d9616ddaf12c500d9d304601eaef923
Ivan <ipkozyrin@gmail.com>
2025-10-07 09:04:07 -0300

    add

--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -1,6 +1,8 @@
+import Tobii from "@midzer/tobii/dist/tobii.modern.js";
+import "@midzer/tobii/dist/tobii.min.css";
+import { tobiiLightboxPlugin } from "vitepress-sls-blog-tmpl/tobiiLightboxPlugin.js";
 import "vitepress-sls-blog-tmpl/blue-theme.css";
 import Layout from "./Layout.vue";
-import { initLightbox } from "vitepress-sls-blog-tmpl/lightbox.js";
 import Theme from "vitepress-sls-blog-tmpl";
 import "vitepress-sls-blog-tmpl/tw-styles.css";
 import "vitepress-sls-blog-tmpl/pagefind-fix.css";
@@ -11,6 +13,6 @@ export default {
   Layout,
   extends: Theme,
   enhanceApp(ctx) {
-    initLightbox(ctx);
+    tobiiLightboxPlugin(ctx, Tobii);
   },
 };
